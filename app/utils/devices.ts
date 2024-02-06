aeb897d7f640c7341ed43530ad0358cf2853fcb2
jingzouzou <827088092@qq.com>
2024-02-06 17:46:17 +0800

    chore: delete unuse file

--- app/utils/devices.ts
+++
@@ -1,8 +0,0 @@
-export const getDeviceInfo = async () => {
-  try {
-    const deviceName = await Device.getDeviceName();
-    console.log('Device Name:', deviceName);
-  } catch (error) {
-    console.error('Error getting device info:', error);
-  }
-};
